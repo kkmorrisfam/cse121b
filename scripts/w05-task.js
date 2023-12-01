3f3b961c051f1b16e8ffe4d373f36ca31970eb58
kkmorrisfam <kkmorrisfam@hotmail.com>
2023-11-30 22:04:30 -0800

    working with alpha sort

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,14 +1,26 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-let templesElement = document.getElementById('temples');
+const templesElement = document.getElementById('temples');
 let templeList = [];
+
+const sorted = (temples) => {
+    let sorted = temples.sort( (a,b) => {
+        if (a.templeName > b.templeName)
+            return 1;
+        else if (a.templeName < b.templeName)
+            return -1;
+        else
+            return 0;
+    });
+    return sorted;
+}
 
 /* async displayTemples Function */
  const displayTemples  = (temples) => {   //pass in an array of objects, within this scope referred to as temples
-    console.log(temples);
-    console.log(templesElement);
-    console.log("displayTemples start->")
+    // console.log(temples);
+    // console.log(templesElement);
+    // console.log("displayTemples start->")
     temples.forEach((temple) => {       //for each element in the array, within this scope identified as temple
         let article = document.createElement('article'); 
         let h3Element = document.createElement('h3');
@@ -20,8 +32,8 @@ let templeList = [];
         article.appendChild(imgElement);
         templesElement.appendChild(article);               
     });
-    console.log("after forEach in displayTemples");
-    console.log(templesElement); 
+    // console.log("after forEach in displayTemples");
+    // console.log(templesElement); 
 };
 
 
@@ -29,23 +41,12 @@ let templeList = [];
 
 const getTemples = async () => {      //pass in zero parameters, just do something, but use async and await
     const response = await fetch('https://byui-cse.github.io/cse121b-ww-course/resources/temples.json');
-    templeList = await response.json();
-    // console.log(JSON.stringify(templeList));
-    displayTemples(templeList);
+    templeList = await response.json();    
+    displayTemples(sorted(templeList));
 }
 
 /* reset Function */
-const reset = (templesElement) => {              //pass in html collection (not array), remove child elements
-    // let parent = document.getElementById("temples");  //one element with ID temples
-    // console.log("temples (reset):  " + temples);
-    
-    // let child = parent.getElementsByTagName('article');
-    // // let gchild = parent.child.querySelector('h3');
-    // console.log("child(reset) : " + child);
-    // parent.remove(child);   //removeChild did not work.
-    // // child.remove(gchild);
-    // console.log("child2 (reset) : " + child);
-    // console.log("temples Element (reset) : " + templesElement);
+const reset = (templesElement) => {              //pass in html collection (not array), remove child elements  
     templesElement.innerHTML = "";    
 }
 
@@ -55,19 +56,26 @@ const reset = (templesElement) => {              //pass in html collection (not 
 /* sortBy Function */
 const sortBy = function(temples) {    //eventually passes in the original Temples List array of objects.
     reset(templesElement);              //passing in the html elements that were created.
-    console.log(templesElement);
+    // console.log(templesElement);
     let filter = document.getElementById('sortBy').value;
-    console.log("filter (sortBy): " + filter);  // shows what the vaule of the drop-down list is, that is chosen.
-    switch(filter) {
-        case "utah":
-        console.log("switch Utah in sortBy/temples" + temples)  //includes all temples here
-        let filteredArray = temples.filter( t => t.location.includes("Utah"));
-        console.log(filteredArray);  //now includes 10 temples
-        displayTemples(filteredArray);           
-        
-        break;
-    }
+    // console.log("filter (sortBy): " + filter);  // shows what the vaule of the drop-down list is, that is chosen.
+    let switchArray = [];
 
+    switch(filter) {
+        case "utah":        
+            switchArray = temples.filter(t => t.location.includes("Utah"));               
+            break;
+        case "notutah":
+            switchArray = temples.filter(t => !(t.location.includes("Utah")));            
+            break;
+        case "older":            
+            switchArray = temples.filter((t) => (new Date(t.dedicated) < new Date(1950, 0, 1))); 
+            break;
+        default:  //all
+            switchArray = temples;
+    }   
+
+    displayTemples(sorted(switchArray));
 }
 
 getTemples();
