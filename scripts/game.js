256b439a9a4f7f584ba8d435d979b1a78881ae09
kkmorrisfam <kkmorrisfam@hotmail.com>
2023-12-07 13:38:13 -0800

    works

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,5 @@
-/*  - How do I keep track of the correct answer to check for correct answer?  It's in the variable question.answer
-How do I test for the correct answer/index number?
-Each possible answer is in a button.  If Button clicked, see if clicked option index == question.answer
-If they match, show Correct message in anouncement div, else, show Incorrect message in announcment div
-Click on next button for next question.
+/* Week 6 Project - Javascript
+Kerri Morris
 */
 
 //script for trivia game project week 6
@@ -10,30 +7,32 @@ let triviaList = [];
 let questions = null;
 let myQuestions = null;
 let currentQuestion = {}; //single object
+const correctHTML = `<h3>Your Answer Is Correct!</h3>
+                    <p>Click the Next Button for a new question</p>`;
+const incorrectHTML = `<h3>Sorry, That's not the answer.</h3>
+                        <p>Click the Next Button for a new question</p>`;
 
 const questionElement = document.getElementById('question');
-const answerElement = document.getElementById('answers');
+const answerElement = document.getElementById('results');
 const buttonsCollection = document.querySelectorAll('.button-answer');    
 const resultsElement = document.getElementById('results');
-
+const choiceElement1 = document.getElementById('btn1');
+const choiceElement2 = document.getElementById('btn2');
+const choiceElement3 = document.getElementById('btn3');
+const choiceElement4 = document.getElementById('btn4');
 
 // let results = null;
-// let url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple";  //currently just pulling 10 questions.
-let url = "games.json";  //test with this because the site only allows so many requests per day, and I don't know how to limit calls during testing
+let url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple";  //currently just pulling 10 questions.
+// url for testing
+// let url = "games.json";  //test with this because the site only allows so many requests per day, and I don't know how to limit calls during testing
 
-const getTrivia= async (url) => {      //pass in url parameters, just do something, but use async and await
+const getTrivia = async (url) => {      //pass in url parameters, just do something, but use async and await
     
     const response = await fetch(url)
-    triviaList = await response.json();    //fill array with response
+    triviaList = await response.json();    //fill array with response   
     
-    // console.log("inside getTrivia");
-    // console.log(triviaList);
-    // myQuestions = 
     mapQuestions(triviaList);
-    // console.log("myQuestions: ")
-    // console.log(JSON.stringify(myQuestions)); error with stringify; undefined without it. What is myQuestions ?????
-    console.log("questions array: ")
-    console.log(questions);
+    
     startGame();  // do this here so that it doesn't "start" until we have api data
 }
 
@@ -46,15 +45,13 @@ function mapQuestions(data) {
     //create a new array with map; question + 4 answers in random order where one is correct
     questions = data.results.map((data) => {
         const newMapQuestion = {
-            question: data.question,
-            
+            question: data.question,            
         };
-        // console.log("inside mapQuestion, create")
-        // console.log(newMapQuestion);
         const newMapAnswers = [...data.incorrect_answers];  //new array of incorrect answers from results
+        
         //this sets the number for the correct answer = "answer: 1"
         newMapQuestion.answer = (Math.floor(Math.random() * 4) + 1);  //get a random index between 0 and 3
-        // console.log(newMapQuestion.answer);
+
         //add correct answer into the new array
         //remove zero elements before random index-1 and insert data.correct_answer -- add answer in at a random spot in the array
         newMapAnswers.splice(newMapQuestion.answer - 1, 0, data.correct_answer);  
@@ -70,53 +67,48 @@ function mapQuestions(data) {
 
 function displayQuestions(currentQuestion) {
     //get element to display questions, add html with question
-    // console.log("inside displayQuestions");
-    // console.log(currentQuestion.question);
-    // console.log(currentQuestion.answer);
-    // console.log(currentQuestion.option1);
-    // console.log(currentQuestion.option2);
-    // console.log(currentQuestion.option3);
-    // console.log(currentQuestion.option4);
+    
     questionElement.innerHTML = `<h2>${currentQuestion.question}</h2>`;    
-    buttonElement.innerHTML = `<p>${currentQuestion.option}`;
+    choiceElement1.innerHTML = `<p>${currentQuestion.option1}`;
+    choiceElement2.innerHTML = `<p>${currentQuestion.option2}`;
+    choiceElement3.innerHTML = `<p>${currentQuestion.option3}`;
+    choiceElement4.innerHTML = `<p>${currentQuestion.option4}`;
+    // console.log(currentQuestion);
 
     Array.from(buttonsCollection).forEach( (button) => {
-        let buttonElement = document.querySelector(button);
+        let buttonElement = document.querySelector(button.button);
         
-        button.addEventListener('click', function() {
-            console.log(this.value);
+        button.addEventListener('click', function() {  
+            // console.log(this.value);          
+            if (this.value == currentQuestion.answer) {
+                answerElement.innerHTML = correctHTML;
+            } else {
+                answerElement.innerHTML = incorrectHTML;
+            };
+            
+            //not the best option, but works for now
+            reset(choiceElement1);
+            reset(choiceElement2);
+            reset(choiceElement3);
+            reset(choiceElement4);
+
         });
-    });
+    });   
 
-    
 
-    /* for i, starting at index 2 - index 5, i++
-    add element to new array
-    with new array. 
-    add textContent to button
-    then, for ech button element, add an eventListener
-
-    if (this.value == currentQuestion.answer)
-        set html to "correct"
-        else set html to "incorrect"
-    
-    */
-        
-
-   
-    // get element to display answers, add buttons, add html to buttons
 }
 
 function getNewQuestion(questions) {
     
     const questionIndex = Math.floor(Math.random() * questions.length);
     currentQuestion =questions[questionIndex];
-    console.log("currentQuestion");
-    console.log(currentQuestion);
+    
+    questions.splice(questionIndex, 1);
 }
 
+
 /* reset Function */
-const reset = (a) => {              //pass in html collection (not array), remove child elements  
+const reset = (a) => {              //pass in element, clear html
     a.innerHTML = "";    
 }
  
@@ -127,5 +119,12 @@ function startGame() {
 
 }
 
-getTrivia(url);
+// getTrivia(url);
 // displayQuestions(questions);
+//start gets a whole new set of questions
+document.getElementById('btnStart').addEventListener('click', () => {getTrivia(url)});
+//next gets the next question in the array of reformatted questions.
+document.getElementById('btnNext').addEventListener('click', () => {
+    getNewQuestion(myQuestions);
+    displayQuestions(currentQuestion);
+});
