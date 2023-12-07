8c8d61193b77c2eb7bf217edbdcee3be462f99ec
kkmorrisfam <kkmorrisfam@hotmail.com>
2023-12-06 20:30:26 -0800

    still working

--- scripts/game.js
+++ scripts/game.js
@@ -8,13 +8,18 @@ Click on next button for next question.
 //script for trivia game project week 6
 let triviaList = [];
 let questions = null;
+let myQuestions = null;
+let currentQuestion = {}; //single object
+
 const questionElement = document.getElementById('question');
 const answerElement = document.getElementById('answers');
+const buttonsCollection = document.querySelectorAll('.button-answer');    
 const resultsElement = document.getElementById('results');
-let myQuestions = null;
+
 
 // let results = null;
-let url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple";  //currently just pulling 10 questions.
+// let url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple";  //currently just pulling 10 questions.
+let url = "games.json";  //test with this because the site only allows so many requests per day, and I don't know how to limit calls during testing
 
 const getTrivia= async (url) => {      //pass in url parameters, just do something, but use async and await
     
@@ -24,11 +29,12 @@ const getTrivia= async (url) => {      //pass in url parameters, just do somethi
     // console.log("inside getTrivia");
     // console.log(triviaList);
     // myQuestions = 
-    orderQuestions(triviaList);
+    mapQuestions(triviaList);
     // console.log("myQuestions: ")
     // console.log(JSON.stringify(myQuestions)); error with stringify; undefined without it. What is myQuestions ?????
     console.log("questions array: ")
     console.log(questions);
+    startGame();  // do this here so that it doesn't "start" until we have api data
 }
 
 //sift through results
@@ -36,47 +42,90 @@ const getTrivia= async (url) => {      //pass in url parameters, just do somethi
 // 0:correct_answer
 // 0:incorrect_answers[0,1,2]
 
-function orderQuestions(data) {
+function mapQuestions(data) {
     //create a new array with map; question + 4 answers in random order where one is correct
     questions = data.results.map((data) => {
-        const newResultQuestion = {
+        const newMapQuestion = {
             question: data.question,
             
         };
-        // console.log("inside OrderQuestion, create")
-        // console.log(newResultQuestion);
-        const newResultAnswers = [...data.incorrect_answers];  //new array of incorrect answers from results
+        // console.log("inside mapQuestion, create")
+        // console.log(newMapQuestion);
+        const newMapAnswers = [...data.incorrect_answers];  //new array of incorrect answers from results
         //this sets the number for the correct answer = "answer: 1"
-        newResultQuestion.answer = (Math.floor(Math.random() * 4) + 1);  //get a random index between 0 and 3
-        console.log(newResultQuestion.answer);
+        newMapQuestion.answer = (Math.floor(Math.random() * 4) + 1);  //get a random index between 0 and 3
+        // console.log(newMapQuestion.answer);
         //add correct answer into the new array
         //remove zero elements before random index-1 and insert data.correct_answer -- add answer in at a random spot in the array
-        newResultAnswers.splice(newResultQuestion.answer - 1, 0, data.correct_answer);  
+        newMapAnswers.splice(newMapQuestion.answer - 1, 0, data.correct_answer);  
 
-        newResultAnswers.forEach((option, index) => {
-            newResultQuestion['option' + (index + 1)] = option;
+        newMapAnswers.forEach((option, index) => {
+            newMapQuestion['option' + (index + 1)] = option;
         });     
-        return newResultQuestion   //return new array into questions
+        return newMapQuestion   //return new array into questions
     } //end of anon function within map
 
     )  //end of map function
 }
 
-function displayQuestions(question) {
+function displayQuestions(currentQuestion) {
     //get element to display questions, add html with question
-    question.forEach(
-        console.log(question.question);
-        questionElement.innerHTML = "<h2>${question.question}</h2>";
-        );
-    //get element to display answers, add buttons, add html to buttons
+    // console.log("inside displayQuestions");
+    // console.log(currentQuestion.question);
+    // console.log(currentQuestion.answer);
+    // console.log(currentQuestion.option1);
+    // console.log(currentQuestion.option2);
+    // console.log(currentQuestion.option3);
+    // console.log(currentQuestion.option4);
+    questionElement.innerHTML = `<h2>${currentQuestion.question}</h2>`;    
+    buttonElement.innerHTML = `<p>${currentQuestion.option}`;
+
+    Array.from(buttonsCollection).forEach( (button) => {
+        let buttonElement = document.querySelector(button);
+        
+        button.addEventListener('click', function() {
+            console.log(this.value);
+        });
+    });
+
+    
+
+    /* for i, starting at index 2 - index 5, i++
+    add element to new array
+    with new array. 
+    add textContent to button
+    then, for ech button element, add an eventListener
+
+    if (this.value == currentQuestion.answer)
+        set html to "correct"
+        else set html to "incorrect"
+    
+    */
+        
+
+   
+    // get element to display answers, add buttons, add html to buttons
+}
+
+function getNewQuestion(questions) {
+    
+    const questionIndex = Math.floor(Math.random() * questions.length);
+    currentQuestion =questions[questionIndex];
+    console.log("currentQuestion");
+    console.log(currentQuestion);
 }
 
 /* reset Function */
 const reset = (a) => {              //pass in html collection (not array), remove child elements  
     a.innerHTML = "";    
 }
+ 
+function startGame() {    
+    myQuestions = [...questions];  //create a copy of questions array, not a pointer to array
+    getNewQuestion(myQuestions);   //is this where I want to call this function?
+    displayQuestions(currentQuestion);
 
-
+}
 
 getTrivia(url);
-displayQuestions(questions);
+// displayQuestions(questions);
