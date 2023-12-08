256cef052c9af6edd25bf08688600494531542ad
kkmorrisfam <kkmorrisfam@hotmail.com>
2023-12-08 07:53:08 -0800

    update

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,5 @@
 /* Week 6 Project - Javascript
+CSE 121B
 Kerri Morris
 */
 
@@ -7,6 +8,9 @@ let triviaList = [];
 let questions = null;
 let myQuestions = null;
 let currentQuestion = {}; //single object
+let amountCorrect = 0;
+let totalAnswered = 0;
+
 const correctHTML = `<h3>Your Answer Is Correct!</h3>
                     <p>Click the Next Button for a new question</p>`;
 const incorrectHTML = `<h3>Sorry, That's not the answer.</h3>
@@ -21,10 +25,12 @@ const choiceElement2 = document.getElementById('btn2');
 const choiceElement3 = document.getElementById('btn3');
 const choiceElement4 = document.getElementById('btn4');
 
+
+
 // let results = null;
-let url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple";  //currently just pulling 10 questions.
+// let url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple";  //currently just pulling 10 questions.
 // url for testing
-// let url = "games.json";  //test with this because the site only allows so many requests per day, and I don't know how to limit calls during testing
+let url = "games.json";  //test with this because the site only allows so many requests per day, and I don't know how to limit calls during testing
 
 const getTrivia = async (url) => {      //pass in url parameters, just do something, but use async and await
     
@@ -58,16 +64,17 @@ function mapQuestions(data) {
 
         newMapAnswers.forEach((option, index) => {
             newMapQuestion['option' + (index + 1)] = option;
-        });     
+        });
+        
         return newMapQuestion   //return new array into questions
     } //end of anon function within map
-
+    
     )  //end of map function
 }
 
 function displayQuestions(currentQuestion) {
     //get element to display questions, add html with question
-    
+         
     questionElement.innerHTML = `<h2>${currentQuestion.question}</h2>`;    
     choiceElement1.innerHTML = `<p>${currentQuestion.option1}`;
     choiceElement2.innerHTML = `<p>${currentQuestion.option2}`;
@@ -77,13 +84,17 @@ function displayQuestions(currentQuestion) {
 
     Array.from(buttonsCollection).forEach( (button) => {
         let buttonElement = document.querySelector(button.button);
+        console.log("forEach" + amountCorrect);
         
         button.addEventListener('click', function() {  
             // console.log(this.value);          
             if (this.value == currentQuestion.answer) {
+                amountCorrect ++;
                 answerElement.innerHTML = correctHTML;
+                console.log("e-click" + amountCorrect);
+                                
             } else {
-                answerElement.innerHTML = incorrectHTML;
+                answerElement.innerHTML = incorrectHTML;                
             };
             
             //not the best option, but works for now
@@ -91,11 +102,10 @@ function displayQuestions(currentQuestion) {
             reset(choiceElement2);
             reset(choiceElement3);
             reset(choiceElement4);
-
+            
         });
-    });   
-
-
+        console.log("after e-click" + amountCorrect);
+    }); 
 }
 
 function getNewQuestion(questions) {
@@ -114,6 +124,9 @@ const reset = (a) => {              //pass in element, clear html
  
 function startGame() {    
     myQuestions = [...questions];  //create a copy of questions array, not a pointer to array
+    console.log("start game " + amountCorrect);
+    amountCorrect = 0;
+    totalAnswered = questions.length;
     getNewQuestion(myQuestions);   //is this where I want to call this function?
     displayQuestions(currentQuestion);
 
@@ -122,9 +135,20 @@ function startGame() {
 // getTrivia(url);
 // displayQuestions(questions);
 //start gets a whole new set of questions
-document.getElementById('btnStart').addEventListener('click', () => {getTrivia(url)});
+document.getElementById('btnStart').addEventListener('click', () => {
+    getTrivia(url);
+    reset(resultsElement);
+});
 //next gets the next question in the array of reformatted questions.
 document.getElementById('btnNext').addEventListener('click', () => {
-    getNewQuestion(myQuestions);
-    displayQuestions(currentQuestion);
+    if (myQuestions.length > 0) {
+        console.log(myQuestions.length);
+        console.log(amountCorrect, totalAnswered);
+        getNewQuestion(myQuestions);
+        displayQuestions(currentQuestion);
+    } else {
+        console.log(myQuestions.length);
+        // console.log(amountCorrect, totalAnswered);
+        resultsElement.innerHTML = `<h2>Total Correct: ${amountCorrect} out of ${totalAnswered}</h2>`;
+    }
 });
